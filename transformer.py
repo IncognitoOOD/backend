093f34a3760a9d94d3b6a0d19ae673e13062a553
m-moslemifar <m.r.moslemifar@gmail.com>
2022-05-16 13:04:05 +0430

    implement transformer with data capsule as a dict, TODO implement data capsule and use it in transformer

--- transformer.py
+++ transformer.py
@@ -1,4 +1,5 @@
 from data_capsule import DataCapsule
+import requests
 
 
 class Transformer:
@@ -8,17 +9,70 @@ class Transformer:
         self.config = config
 
     def __rename(self, dc: DataCapsule):
-        # do some stuff
+        rename = self.config['rename']
+        if rename:
+            for r in rename:
+                old_name = list(r.keys())[0]
+                new_name = list(r.values())[0]
+                dc[new_name] = dc[old_name]
+                del dc[old_name]
+
         return dc
 
     def __concat(self, dc: DataCapsule):
-        # do some stuff
+        concat = self.config['concat']
+        if concat:
+            for c in concat:
+                dest_field = list(c.keys())[0]
+                src_field_1 = c['dest_field'][0]
+                src_field_2 = c['dest_field'][1]
+                sep = c['sep']
+                dc[dest_field] = str(c[src_field_1]) + sep + str(c[src_field_2])
+                del c[src_field_1]
+                del c[src_field_2]
+
         return dc
 
     def __api_call(self, dc: DataCapsule):
-        # do some stuff
+        api_call = self.config['api_call']
+        if api_call:
+            for a in api_call:
+                dest_field = a['dest_field']
+                api_address = a['api_address']
+                method = a['method']
+
+                args = a['args']
+                params = dict()
+                if args:
+                    for arg in args:
+                        arg_name = arg['arg_name']
+                        if arg['data']['type'] == 'FROM_DATA_CAPSULE':
+                            data = dc[arg['data']['src_field']]
+                        else:
+                            data = arg['data']['value']
+                        params[arg_name] = data
+
+                if method == 'GET':
+                    try:
+                        response = requests.get(url=api_address, params=params)
+                    except Exception:
+                        raise Exception('API returned no response')
+                else:
+                    try:
+                        response = requests.post(url=api_address, params=params)
+                    except Exception:
+                        raise Exception('API returned to response')
+
+                dc[dest_field] = response.text
+
         return dc
 
     def run(self, dc_list: list):
-        # do some stuff
+        ret_list = list()
+        for dc in dc_list:
+            renamed = self.__rename(dc)
+            concatenated = self.__concat(renamed)
+            api_called = self.__api_call(concatenated)
+            ret_list.append(api_called)
+
         return dc_list
