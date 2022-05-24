081fc5da0032ded300c4af2ac643bcb8f5f44de2
safdarian <safdarian2000@gmail.com>
2022-05-24 13:47:29 +0430

    roll back data_capsule to simple implementation

--- data_capsule.py
+++ data_capsule.py
@@ -1,28 +1,3 @@
 class DataCapsule:
-    fields = set()
-
-    def __init__(self, doc_id, version=1, document=None, process_data=None, metadata=None, logs=None,
-                 id_field_name=None,
-                 fields_update=None):
-        if metadata is None:
-            metadata = {}
-        if process_data is None:
-            process_data = {}
-        self.version = version
+    def __init__(self, document=None):
         self.document = document
-        [self.fields.add(x) for x in self.document.keys()]
-        self.process_data = process_data
-        self.metadata = metadata
-        self.logs = logs
-        self.transform_meta = dict()
-        self.doc_id = doc_id
-        self.id_field_name = id_field_name
-        self.fields_update = fields_update
-
-    def add_process_data(self, process_data):
-        self.process_data.update(process_data)
-
-    def update_fields(self):
-        self.fields = set()
-        [self.fields.add(x) for x in self.document.keys()]
-        [self.fields.add(x) for x in self.process_data.keys()]
