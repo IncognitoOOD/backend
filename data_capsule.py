3756923b9836b98cc7f8d149812abec36a3a1de8
safdarian <safdarian2000@gmail.com>
2022-05-29 14:15:34 +0430

    add datacapsule beautiful_print

--- data_capsule.py
+++ data_capsule.py
@@ -1,3 +1,8 @@
+import json
+
 class DataCapsule:
     def __init__(self, document=None):
         self.document = document
+
+    def beautiful_print(self):
+        print(json.dumps(self.document, indent=4))
