8355c6e1e1f135b946b6c1a988aa9891b9f5d819
safdarian <safdarian2000@gmail.com>
2022-06-01 14:25:17 +0430

    add fields to data_capsule.py

--- data_capsule.py
+++ data_capsule.py
@@ -1,8 +1,11 @@
 import json
 
+
 class DataCapsule:
-    def __init__(self, document=None):
+    def __init__(self, document=None, fields=None):
         self.document = document
+        self.fields = fields
 
     def beautiful_print(self):
+        print("fields:", self.fields)
         print(json.dumps(self.document, indent=4))
