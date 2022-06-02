bdee479808c4715984aaae5069d39f4413ec159f
safdarian <safdarian2000@gmail.com>
2022-06-02 06:05:10 +0430

    implement manager.py

--- manager.py
+++ manager.py
@@ -4,22 +4,19 @@ from pipeline import Pipeline
 class Manager:
     __pipelines: list
 
-    def __save_log(self, log: dict):
-        # save log
-        pass
-
     def run(self):
         # run manager
-        pass
+        while True:
+            for pipeline in self.__pipelines:
+                if pipeline.should_run():
+                    pipeline.run()
 
     def get_number_of_pipelines(self):
-        return 0
+        return len(self.__pipelines)
 
     def add_pipeline(self, full_config: dict):
         # add pipeline
-        pass
+        self.__pipelines.append(Pipeline(full_config))
 
     def test_pipeline_config(self, full_config: dict):
-        pipeline = Pipeline(full_config)
-        # do some stuff
-        pass
+        return pipeline.test_pipeline_config(full_config)
