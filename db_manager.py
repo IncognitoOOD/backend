704eefda0a6df3d97282d4d11e178773a5080dcd
safdarian <safdarian2000@gmail.com>
2022-06-11 17:08:44 +0430

    add mongodb and mongodb_manager

---
+++ db_manager.py
@@ -0,0 +1,38 @@
+import json
+import pymongo
+
+
+class MongoManager:
+    def __init__(self):
+        self.mongo_config = json.loads(open("db_configs/mongo_config.json", "r").read())
+        d = {
+            "host":       self.mongo_config["host"],
+            "port":       self.mongo_config["port"],
+            "username":   self.mongo_config["username"],
+            "password":   self.mongo_config["password"],
+            "database":   self.mongo_config["database"],
+            "collection": self.mongo_config["collection"]
+        }
+        self.client = pymongo.MongoClient("mongodb://{}:{}/".format(d["host"], str(d["port"])), username=d["username"], password=d["password"])
+        self.mydb = self.client[d["database"]]
+        self.configs = self.mydb[d["collection"]]
+
+    def select(self):
+        result = self.configs.find()
+        return [item for item in result]
+
+    def insert(self, document: dict):
+        self.configs.insert_one(document)
+
+    def search_by_condition(self, conditions: dict):
+        result = self.configs.find(conditions)
+        return [item for item in result]
+
+
+if __name__ == "__main__":
+    db = MongoManager()
+    print(db.select())
+    db.insert({"name": "Till", "family": "Lindemann"})
+    print(db.select())
+    print(db.search_by_condition({"name": "reznov"}))
+
