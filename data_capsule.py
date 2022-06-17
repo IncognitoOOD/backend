b9b3ad41d35891913b2dedaa356af5d17dbb0fdf
safdarian <safdarian2000@gmail.com>
2022-06-17 12:32:24 +0430

    add creqte from json on init method

--- data_capsule.py
+++ data_capsule.py
@@ -1,14 +1,18 @@
 import json
+from mimetypes import init
 
 
 class DataCapsule:
-    def __init__(self, document=None, fields=None):
-        if fields is None:
-            fields = []
-        if document is None:
-            document = {}
-        self.document = document
-        self.fields = fields
+    def __init__(self, document=None, fields=None, json_data=None):
+        if json_data:
+            self.create_from_json(json_data)
+        else:
+            if fields is None:
+                fields = []
+            if document is None:
+                document = {}
+            self.document = document
+            self.fields = fields
 
     def create_from_json(self, j):
         self.document = j['document']
