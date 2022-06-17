a7462f340ad41d1424f8746a4d3b30b2ab81a519
safdarian <safdarian2000@gmail.com>
2022-06-17 05:43:27 +0430

    add json to DataCapsule

--- data_capsule.py
+++ data_capsule.py
@@ -10,6 +10,16 @@ class DataCapsule:
         self.document = document
         self.fields = fields
 
+    def create_from_json(self, j):
+        self.document = j['document']
+        self.fields = j['fields']
+
+    def get_json(self):
+        d = {
+            "fields": self.fields,
+            "document": self.document
+        }
+        return d
     def beautiful_print(self):
         print("fields:", self.fields)
         print(json.dumps(self.document, indent=4))
