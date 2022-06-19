eb8d99cc32fff069cbf1f94afc97087e49f85767
safdarian <safdarian2000@gmail.com>
2022-06-19 13:22:18 +0430

    fix mongodb

--- Databases/mongo.py
+++ Databases/mongo.py
@@ -11,7 +11,14 @@ class MongoDB:
             "database":   self.config["database"],
             "collection": self.config["collection"]
         }
-        self.client = pymongo.MongoClient(**d)
+        pass_d = {
+            "host":       self.config["host"],
+            "port":       self.config["port"]
+        }
+        if "username" in self.config and "password" in self.config:
+            pass_d["username"] = self.config["username"]
+            pass_d["password"] = self.config["password"]
+        self.client = pymongo.MongoClient(**pass_d)
         self.mydb = self.client[d["database"]]
         self.collection = self.mydb[d["collection"]]
 
